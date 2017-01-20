d4774956d7e504e3f50b9e9c408ddaf58d4f6f12
cexwasp2010 <cesar.warrior@hotmail.com>
2017-01-20 09:42:30 -0500

    Initial Commit

---
+++ WebAsp.Net_Primeros_Pasos/WebAsp.Net_Primeros_Pasos/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebAsp.Net_Primeros_Pasos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
